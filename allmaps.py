e671e6941d8cc8fdab41c6322db7b880b42dde2e
Rhidalin Bytes <brian@markland-ohio.com>
2010-07-08 04:46:29 -0400

    Change !maps and !nextmap for Openwarfare load based maps
    Signed-off-by: Rhidalin Bytes <brian@markland-ohio.com>

---
+++ allmaps.py
@@ -0,0 +1,146 @@
+#All maps plugin
+#cmd is !maps or !maps low, !maps med, !maps high, !maps all
+
+__version__ = '1.0'
+__author__  = 'Rhidalin_Bytes'
+
+import b3, re, string
+import b3.events
+
+class AllmapsPlugin(b3.plugin.Plugin):
+    _adminPlugin = None
+
+    def startup(self):
+      """\
+      Initialize plugin settings
+      """
+      self._adminPlugin = self.console.getPlugin('admin')
+      if not self._adminPlugin:
+        self.error('Could not find admin plugin')
+        return False
+    
+      if 'commands' in self.config.sections():
+        for cmd in self.config.options('commands'):
+          level = self.config.get('commands', cmd)
+          sp = cmd.split('-')
+          alias = None
+          if len(sp) == 2:
+            cmd, alias = sp
+
+          func = self.getCmd(cmd)
+          if func:
+            self._adminPlugin.registerCommand(self, cmd, level, func, alias)
+            
+      self.debug('Allmaps Started')
+
+
+    def getCmd(self, cmd):
+      cmd = 'cmd_%s' % cmd
+      if hasattr(self, cmd):
+        func = getattr(self, cmd)
+        return func
+
+      return None
+
+    def cmd_maps(self, data, client=None, cmd=None):
+        """\
+        <rotation> - For current rotation enter nothing or low, med, high or all for others.
+        """
+        population = {'low':'sv_mapRotation_low','med':'sv_mapRotation_medium','high':'sv_mapRotation_high'}
+
+        if not self._adminPlugin.aquireCmdLock(cmd, client, 60, True):
+            client.message('^7Do not spam commands')
+            return
+
+        if not data:
+            maps = self.getMaps(self.rotation())
+            data = "current"
+        elif not data =='all':
+            try:
+                self.debug ('population = %s' % population[data])
+                maps = self.getMaps(population[data])
+            except:
+                client.message('There was a problem getting %s' % population[data])
+        else:
+            try:
+            maps = []
+                for n in population:
+                    self.debug ('population = %s' % population[n])
+                    maps = maps + self.getMaps(population[n])
+            except:
+                client.message('There was a problem getting all maps')
+        if maps:
+            cmd.sayLoudOrPM(client, '^7Map Rotation(^2%s^7): ^2%s' % (data, string.join(maps, '^7, ^2')))
+        else:
+            client.message('You got no maps dummy')
+
+    def cmd_nextmap(self, data, client=None, cmd=None):
+        """\
+        - list the next map in rotation
+        """
+        if not self._adminPlugin.aquireCmdLock(cmd, client, 60, True):
+            client.message('^7Do not spam commands')
+            return
+
+        map = self.getNextMap()
+        if map:
+            cmd.sayLoudOrPM(client, '^7Next Map: ^2%s' % map)
+        else:
+            client.message('^7Error: could not get map list')
+            
+    _reMap = re.compile(r'map ([a-z0-9_-]+)', re.I)
+    def getMaps(self, maploc='sv_mapRotation'):
+        maps = self.console.getCvar(maploc)
+
+        nmaps = []
+        if maps:
+            maps = re.findall(self._reMap, maps[0])
+
+            for m in maps:
+                if m[:3] == 'mp_':
+                    m = m[3:]
+
+                nmaps.append(m.title())
+
+        return nmaps
+    
+    def getNextMap(self):
+        if not self.console.game.mapName: return None
+        
+        maps = self.console.getCvar(self.rotation())
+
+        if maps:
+            maps = re.findall(self._reMap, maps[0])
+
+            gmap = self.console.game.mapName.strip().lower()
+
+            found = False
+            for nmap in maps:
+                nmap = nmap.strip().lower()
+                if found:
+                    found = nmap
+                    break
+                elif nmap == gmap:
+                    # current map, break on next map
+                    found = True
+
+            if found == True:
+                # map is first map in rotation
+                nmap = maps[0].strip().lower()
+
+            if found:
+                if nmap[:3] == 'mp_': nmap = nmap[3:]
+                return nmap.title()
+
+            return None
+        else:
+            return None
+            
+    def rotation(self):
+        MRLB = self.console.getCvar('sv_mapRotationLoadBased')
+        self.debug('sv_mapRotationLoadBased is %s' % MRLB)
+        if MRLB:
+            codmaps = ('sv_mapRotationCurrent')
+        else:
+            codmaps = ('sv_mapRotation')
+        return codmaps
